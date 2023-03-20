428ddb2dc626c97a520aa06a023a2995535b5049
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-03-20 12:17:50 -0600

    use AR to query the DB to return a number of products and categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,5 +2,7 @@ class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with :name => ENV['ADMIN_USERNAME'], :password => ENV['ADMIN_PASSWORD']
 
   def show
+    @total_products = Product.all
+    @total_categories = Category.all
   end
 end
