aff53027845a4b75dadf86692b89b3ebac7dcd39
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-03-28 12:29:13 -0600

    recreate spec folder from scratch

--- spec/helpers/admin/categories_helper_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'rails_helper'
-
-# Specs in this file have access to a helper object that includes
-# the Admin::CategoriesHelper. 
-RSpec.describe Admin::CategoriesHelper, type: :helper do
-  pending "add some examples to (or delete) #{__FILE__}"
-end
