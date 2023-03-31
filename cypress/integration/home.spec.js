fed4923771eb5286d3f1fbd07f07ed18ffbf6713
nicholasburgess17 <nicholas.burgess13@gmail.com>
2023-03-31 13:45:58 -0600

    create initial homepage tests

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,11 @@
+describe('homepage testing', () => {
+    it('should visit the homepage', () => {
+        cy.visit('http://localhost:3000/')
+      });
+      it("There is products on the page", () => {
+        cy.get(".products article").should("be.visible");
+      });
+      it("There is 12 products on the page", () => {
+        cy.get(".products article").should("have.length", 12);
+      });
+    })
